95e469839e95d959b77238abfcf6beb0fbc9c144
d-w-moore <dmoore@renci.org>
2020-02-21 08:25:28 -0500

    first commit

---
+++ run_irods_provider.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+
+## ----      Wait on database           ----
+## -----------------------------------------
+
+while [ $((++x)) -lt 10 ] && ! psql -U postgres -h dbhost -c '\l' >/dev/null 2>&1
+do
+  echo >&2 ... waiting on db server ... "($x)"
+  sleep 1
+done
+
+
+### ---- Set up db for ICAT ----
+### -----------------------------------------
+#
+psql -U postgres -h dbhost -f /db_irods_setup.txt
+
+
+## ---- Configure (or Run) irods server ----
+## -----------------------------------------
+
+if ! id -u irods >/dev/null 2>&1
+then
+
+  echo >&2 "Configuring and starting iRODS ..."
+  python /var/lib/irods/scripts/setup_irods.py < /dbhost_setup_postgres.input
+
+else
+
+  echo >&2 "Starting iRODS ..."
+  service irods start
+
+fi && echo >&2  $'\n\t --> IRODS Server is running \n'
+
+#-- set up python rule engine plugin
+#
+#if yum list installed | grep irods-rule-engine-plugin-python >/dev/null 2>&1
+#then
+#    su - irods -c 'cp /etc/irods/core.py.template /etc/irods/core.py' && /jqeditconfig.sh 
+#fi
+
+while true
+do
+  sleep 1
+done
+
---
+++ start_pg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+su - postgres -c '/usr/bin/pg_ctl -D /var/lib/pgsql/data -l logfile start'
+
+while : 
+do 
+  echo '*'; sleep 5; 
+done >/tmp/log_output.$$  
